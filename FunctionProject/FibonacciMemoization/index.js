011da8730845c47c71434a7a20a29bd3db43d40c
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2020-11-09 22:03:28 -0500

    Images

--- FunctionProject/FibonacciMemoization/index.js
+++ FunctionProject/FibonacciMemoization/index.js
@@ -1,38 +1,31 @@
 var bigInt = require("big-integer");
-let fibonacciList = []
+let fibonacciMap = new Map()
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     let nth = req.body.nth
     let nth_1 = bigInt.one;
-    let nth_2 = bigInt.zero;
+    let nth_0 = bigInt.zero;
     let answer = bigInt.zero;
 
     let fibonacci = (n) =>{
-        if (fibonacciList[n] != -1){
-            return fibonacciList[n];
-        }else{
-            if (n === 0){
-                return nth_2;
-            }
-            else if (n === 1){
-                return nth_1;
-            }else{
-                fibonacciList[n] = fibonacci(n-1).add(fibonacci(n-2));
-                return fibonacciList[n];
-            }
-        }
-        
+        if(!fibonacciMap.has(n) && fibonacciMap.has(n-1)){
+            fibonacciMap.set(n,fibonacciMap.get(n-1).add(fibonacciMap.get(n-2)));
+        }else if (!fibonacciMap.has(n)){
+            fibonacciMap.set(n,fibonacci(n-1).add(fibonacci(n-2)));
+        }return fibonacciMap.get(n);
     }
+        
     if (nth < 0)
         throw 'must be greater than 0'
     else{
-        if (nth <= fibonacciList.length){
-            answer = fibonacciList[nth];
+        if (fibonacciMap.has(nth)){
+            answer = fibonacciMap.get(nth);
         }
         else{
-            fibonacciList = []
-            fibonacciList = new Array(nth+1).fill(-1);
+            fibonacciMap = new Map()
+            fibonacciMap.set(0,nth_0);
+            fibonacciMap.set(1,nth_1);
             answer = fibonacci(nth);
         }
         
